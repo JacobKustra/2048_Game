cd1eba1d9d1b0d596fee0b76f797650b94faaed4
Jacob Kustra <jacobkustra@gmail.com>
2025-07-04 21:44:52 -0700

    Grid added below tiles, so when tiles move they don't clip, also added proper tile drawing size & location

--- src/play2048/game.py
+++ src/play2048/game.py
@@ -6,8 +6,8 @@ def play_game():
     
     clock = pygame.time.Clock() 
 
-    screen_width = 800
-    screen_height = 800
+    screen_width = 750
+    screen_height = 750
     screen = pygame.display.set_mode((screen_width, screen_height))
 
     # Game States
@@ -26,10 +26,11 @@ def play_game():
 
     grid = 4
     cell_size = (screen_width / grid)
+    tile_size = ((screen_width - 50) / grid)
 
     running = True
 
-    player_pos = [[2,2], [0,1]]
+    player_pos = [[2,2], [0, 0], [1, 2], [3,3], [3, 2], [1, 1]]
 
     while running:
 
@@ -58,6 +59,8 @@ def play_game():
                 game_state = AWAITING_INPUT
 
 
+        
+
         for i in range(grid):
             temp_x = i * cell_size
             for j in range(grid):
@@ -73,11 +76,33 @@ def play_game():
                     else:
                         pygame.draw.rect(screen, "white", (temp_x, temp_y, cell_size, cell_size))
 
-        for x in player_pos:
-            player_x = x[0] * cell_size
-            player_y = x[1] * cell_size
-            pygame.draw.rect(screen, "red", (player_x, player_y, cell_size, cell_size))
+
+        # Horizontal Bars
+        pygame.draw.rect(screen, "black", (0, 0, screen_width, 10))
+        pygame.draw.rect(screen, "black", (0, (tile_size + 10), screen_width, 10))
+        pygame.draw.rect(screen, "black", (0, (2 * (tile_size + 10)), screen_width, 10))
+        pygame.draw.rect(screen, "black", (0, (3 * (tile_size + 10)), screen_width, 10))
+        pygame.draw.rect(screen, "black", (0, (screen_height - 10), screen_width, 10))
+
+        # Vertical Bars
+        pygame.draw.rect(screen, "black", (0, 0, 10, screen_height))
+        pygame.draw.rect(screen, "black", ((tile_size + 10), 0, 10, screen_height))
+        pygame.draw.rect(screen, "black", ((2 * (tile_size + 10)), 0, 10, screen_height))
+        pygame.draw.rect(screen, "black", ((3 * (tile_size + 10)), 0, 10, screen_height))
+        pygame.draw.rect(screen, "black", ((screen_width - 10), 0, 10, screen_height))
         
+        for x in player_pos:
+            player_x = (x[0] * tile_size) + ((x[0] + 1) * 10)
+            player_y = (x[1] * tile_size) + ((x[1] + 1) * 10)
+            pygame.draw.rect(screen, "red", (player_x, player_y, tile_size, tile_size))
+       
+
+        # Position 0, 0 is 10, 10
+        # position 1, 1 is 10 + cell_size
+
+
+        # Move
+
         pygame.display.flip()
 
         clock.tick(60)
